7299a1878083ae4bc1cc440b2b782d40864cd5d8
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2025-10-03 11:33:36 -0500

    Make all dashes the same width

--- src/lib/components/sections/Research.svelte
+++ src/lib/components/sections/Research.svelte
@@ -1,7 +1,7 @@
 <section class="mt-6 text-left">
     <h2 class="text-2xl font-bold mb-2">Selected Research</h2>
     <ul class="list-disc list-inside text-gray-600 space-y-1">
-        <li><strong><a href="https://www.mdpi.com/2227-7102/15/2/198" target="_blank">A Novice-Friendly and Accessible Networked Educational Robotics Simulation Platform - Education Sciences 2025</a></strong>: Most recent case study of simulated robotics platform.</li>
+        <li><strong><a href="https://www.mdpi.com/2227-7102/15/2/198" target="_blank">A Novice-Friendly and Accessible Networked Educational Robotics Simulation Platform — Education Sciences 2025</a></strong>: Most recent case study of simulated robotics platform.</li>
         <li><strong><a href="https://netsblox.org/documents/3328778.3366878.pdf" target="_blank">Simulation for Novice-Friendly Classroom Robotics — Frontiers in Computer Science 2023</a></strong>: Continued exploration of novice-friendly simulated robotics education.</li>
         <li><strong><a href="https://dl.acm.org/doi/10.1145/3328778.3366878" target="_blank">A Hands-On Cybersecurity Curriculum Using a Robotics Platform — SIGCSE 2020</a></strong>: Educational robots as a tool for teaching cybersecurity.</li>
         <li><strong><a href="https://par.nsf.gov/servlets/purl/10322189" target="_blank">Enabling Collaborative Distance Robotics Education for Novice Programmers — VL/HCC 2021</a></strong>: Initial design and implementation of a networked robotics simulation platform for novice learners. </li>
