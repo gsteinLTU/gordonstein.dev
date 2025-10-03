73bdaaaee5f6238bc3931a028651ef4f2ac7e755
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2025-10-03 11:32:53 -0500

    Update head tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,18 @@
 </script>
 
 <svelte:head>
+	<title>Gordon Stein - Software Developer & CS Education Researcher</title>
 	<link rel="icon" href="./fav.png" />
+
+	<!-- Primary Meta Tags -->
+	<meta name="description" content="Gordon Stein's personal website. Software Developer & Computer Science Education Researcher." />
+	<meta name="author" content="Gordon Stein" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:title" content="Gordon Stein - Software Developer & CS Education Researcher" />
+	<meta property="og:description" content="Gordon Stein's personal website. Software Developer & Computer Science Education Researcher." />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://gordonstein.dev" />
 </svelte:head>
 
 {@render children?.()}
